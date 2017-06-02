80a334b371e3ff31486eedbf6605292ad78f88e9
alisinabh <alisina.bm@gmail.com>
2017-06-02 01:36:52 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alisinabh.Mixfile do
 
   def project do
     [app: :alisinabh,
-     version: "0.0.5",
+     version: "0.0.6",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
